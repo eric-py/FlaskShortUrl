bef06785a9057a99ac451e13e815c70f6e7fe62c
Eric <halakoei@yahoo.com>
2024-10-09 16:22:09 +0330

    index route

--- app/views.py
+++ app/views.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash
 from .forms import ShortLinkForm
-from .models import ShortLink
+from .models import ShortLink, db
+import secrets
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 @main.route('/home')
 def home():
-    return render_template('index.html')
+    # show and send form on index files
+    form = ShortLinkForm()
+    return render_template('index.html', form=form)
