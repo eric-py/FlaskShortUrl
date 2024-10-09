b54a31750acd9837b68febb032777b12c27c3e70
Eric <halakoei@yahoo.com>
2024-10-09 12:17:42 +0330

    add secret key to config file

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 import os
 
 class Config:
-    SECRET_KEY = '' # your secret key here
+    SECRET_KEY = '21d5s4da1s5d45asd412a21vds654f6sdf4' # your secret key here
     PERMANENT_SESSION_LIFETIME = timedelta(days=30)
     UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__)) + '/uploads'
     TEMPLATES_FOLDER = os.path.abspath(os.path.dirname(__file__)) + '/templates'
