dda5e585a95b8b38d337ecc06a95f9e02267bae2
Eric <halakoei@yahoo.com>
2024-10-09 12:26:11 +0330

    create form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, URL
 
-# Forms
+# Forms short link
+
+class ShortLinkForm(FlaskForm):
+    long_url = StringField('Enter a URL', validators=[DataRequired(), URL()])
+    submit = SubmitField('Shorten')
